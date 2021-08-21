90728871e6e748edf61062636f5068cade3c7160
HB-fga <hugoricardo.so.be@gmail.com>
2021-08-21 18:28:19 -0300

    Adicionando geração de labirinto com Prim
    Co-authored-by: Lucas Gabriel <lucasbsb100@gmail.com>

--- app.py
+++ app.py
@@ -1,48 +1,24 @@
 import utils 
+
+import heapq
 import pyxel
 import random
-
-# DESCOBRE QUANTAS CELULAS POSSO TER
-# for i in range(1, 21):
-#     print("pixels por celula: ")
-#     print((256-i-1)/i)
-#     print("celulas: ")
-#     print(i)
-#     print()
-
-def rect_custom(x1, y1, x2, y2, color):
-
-    if x1 > x2:
-        x1, x2 = x2, x1
-
-    if y1 > y2:
-        y1, y2 = y2, y1
-
-    pyxel.rect(x1, y1, x2-x1, y2-y1, color)
-
-def coord(x, y):
-    return (str(x) + "-" + str(y))
-
-def inv_coord(coord):
-    st = coord.split("-")
-    return [int(st[0]), int(st[1])]
-     
 
 class Node:
     def __init__(self, node):
         self.id = node
-        self.adjacent = {}
+        self.bros = {}
 
     def __str__(self):
-        return str(self.id) + str([x.id for x in self.adjacent])
+        return str(self.id) + str([x.id for x in self.bros])
 
     # Adiciona vizinho a lista de adjacência
     def add_bro(self, bro, weight=0):
-        self.adjacent[bro] = weight
+        self.bros[bro] = weight
     
-    # Retorna lista de vizinho
+    # Retorna lista de vizinhos
     def get_bros(self):
-        return self.adjacent.keys()  
+        return self.bros.keys()  
 
     # Retorna ID do nó
     def get_id(self):
@@ -50,7 +26,7 @@ class Node:
 
     # Retorna PESO da aresta do nó com o vizinho
     def get_weight(self, neighbor):
-        return self.adjacent[neighbor]
+        return self.bros[neighbor]
 
 class Graph:
     def __init__(self):
@@ -84,28 +60,78 @@ class Graph:
         self.node_dict[frm].add_bro(self.node_dict[to], cost)
         self.node_dict[to].add_bro(self.node_dict[frm], cost)
 
-    # Retorna lista de nós 
+    # Retorna dicionário com nós
     def get_nodes(self):
         return self.node_dict.keys()
-
 
 g = Graph()
 
 for i in range(0, 17):
     for j in range(0, 13):
-        g.add_node(coord(i, j))
+        g.add_node(utils.coord(i, j))
 
 for i in range(0, 17):
     for j in range(0, 13):
         if(i != 0):
-            g.add_edge(coord(i, j), coord(i-1, j), random.randint(1, 20))
+            g.add_edge(utils.coord(i, j), utils.coord(i-1, j), random.randint(1, 20))
         if(j != 0):
-            g.add_edge(coord(i, j), coord(i, j-1), random.randint(1, 20))
+            g.add_edge(utils.coord(i, j), utils.coord(i, j-1), random.randint(1, 20))
         if(i != 16):
-            g.add_edge(coord(i, j), coord(i+1, j), random.randint(1, 20))
+            g.add_edge(utils.coord(i, j), utils.coord(i+1, j), random.randint(1, 20))
         if(j != 12):
-            g.add_edge(coord(i, j), coord(i, j+1), random.randint(1, 20))
+            g.add_edge(utils.coord(i, j), utils.coord(i, j+1), random.randint(1, 20))
 
+
+tree = Graph()
+edges = []
+def paint(current, bro):
+    current_pos = utils.coord_convert(current)
+    bro_pos = utils.coord_convert(bro)
+
+    x1 = current_pos[0]*15+1
+    y1 = current_pos[1]*15+1
+    x2 = bro_pos[0]*15+1
+    y2 = bro_pos[1]*15+1
+    
+    if x1 < x2 or y1 < y2:
+        x2 += 14
+        y2 += 14
+    elif x1 > x2 or y1 > y2:
+        x1 += 14
+        y1 += 14
+
+    utils.rect_custom(x1, y1, x2, y2, 8)
+         
+def buildTree(start):
+    visited = []
+    for i in range(0, 17):
+        for j in range(0, 13):
+            tree.add_node(utils.coord(i, j))
+
+    for edge in edges:
+        tree.add_edge(edge[0], edge[1], 0)
+
+def prim(start):
+    candidatas = []
+    visited = []
+    for u in g.node_dict[start].get_bros():
+        heapq.heappush(candidatas,(g.node_dict[start].get_weight(u), (g.node_dict[start].get_id(), u.get_id())))
+    visited.append(g.node_dict[start].get_id())
+    
+    while len(visited) < g.num_nodes:
+        _ , (node_u,node_v) = heapq.heappop(candidatas)
+
+        if node_v not in visited:
+            visited.append(node_v)
+            edges.append((node_u,node_v))
+            current = node_v
+            for bro in g.node_dict[current].get_bros():
+                heapq.heappush(candidatas,(g.node_dict[current].get_weight(bro), (current, bro.get_id())))
+                
+    buildTree(start)
+
+start = utils.coord(random.randint(0, 16), random.randint(0, 12))
+prim(start)
 
 class App:
     def __init__(self):
@@ -127,4 +153,9 @@ class App:
         for i in range(0, 14):
             pyxel.line(0, 15*i, utils.WIDTH, 15*i, 7)
         
+        for i in range(0, self.delay+1):
+            paint(edges[i][0], edges[i][1])
+
+        if(self.delay!=len(edges)-1):
+            self.delay += 1
 App()
--- utils.py
+++ utils.py
@@ -1,2 +1,49 @@
+import pyxel
+
 WIDTH = 256
 HEIGHT = 196
+
+def rect_custom(x1, y1, x2, y2, color):
+
+    if x1 > x2:
+        x1, x2 = x2, x1
+
+    if y1 > y2:
+        y1, y2 = y2, y1
+
+    pyxel.rect(x1, y1, x2-x1, y2-y1, color)
+
+def coord(x, y):
+    return (str(x) + "-" + str(y))
+
+def coord_convert(coord):
+    st = coord.split("-")
+    return [int(st[0]), int(st[1])]
+
+def cave_paint(current, bro):
+    current_pos = coord_convert(current)
+    bro_pos = coord_convert(bro)
+
+    x1 = current_pos[0]*15+1
+    y1 = current_pos[1]*15+1
+    x2 = bro_pos[0]*15+1
+    y2 = bro_pos[1]*15+1
+    
+    if x1 < x2 or y1 < y2:
+        x2 += 14
+        y2 += 14
+    elif x1 > x2 or y1 > y2:
+        x1 += 14
+        y1 += 14
+
+    rect_custom(x1, y1, x2, y2, 8)
+
+# DEBUG
+
+# DESCOBRE QUANTAS CELULAS POSSO TER
+# for i in range(1, 21):
+#     print("pixels por celula: ")
+#     print((256-i-1)/i)
+#     print("celulas: ")
+#     print(i)
+#     print()
