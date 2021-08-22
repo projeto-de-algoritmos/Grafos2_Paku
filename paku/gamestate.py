c578c08f4c5eb132a4f29f1aa804ecd6c80350ad
HB-fga <hugoricardo.so.be@gmail.com>
2021-08-22 00:11:56 -0300

    adicionando classe player
    Co-authored-by: Lucas Gabriel <lucasbsb100@gmail.com>

--- paku/gamestate.py
+++ paku/gamestate.py
@@ -1,3 +1,81 @@
+import utils
+import prim
+import player
+
+import pyxel
+import random
+
+player1 = player.Player()
+
 class GameState:
-    def __init__(self, state):
-    
+    def __init__(self):
+        self.state = "menu"
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+
+        if(self.state == "menu"):
+            ...
+        if(self.state == "start"):
+            for i in range(0, 17):
+                for j in range(0, 13):
+                    utils.g.add_node(utils.coord_str(i, j))
+
+            for i in range(0, 17):
+                for j in range(0, 13):
+                    if(i != 0):
+                        utils.g.add_edge(utils.coord_str(i, j), utils.coord_str(i-1, j), random.randint(1, 20))
+                    if(j != 0):
+                        utils.g.add_edge(utils.coord_str(i, j), utils.coord_str(i, j-1), random.randint(1, 20))
+                    if(i != 16):
+                        utils.g.add_edge(utils.coord_str(i, j), utils.coord_str(i+1, j), random.randint(1, 20))
+                    if(j != 12):
+                        utils.g.add_edge(utils.coord_str(i, j), utils.coord_str(i, j+1), random.randint(1, 20))
+
+            # if menu var ...
+            start = utils.coord_str(random.randint(0, 16), random.randint(0, 12))
+            utils.tree, utils.edges = prim.prim_maze(utils.g, start, utils.edges)
+
+            player1.atNode = utils.g.get_node("0-0")
+
+            self.state = "prim"
+                
+        elif(self.state == "prim"):
+
+            if(utils.delay!=len(utils.edges)-1):
+                utils.delay += 1
+            else:
+                self.state = "run"
+
+        elif(self.state == "run"):
+            player1.update()
+        elif(self.state == "game_over"):
+            ...
+
+    def draw(self):
+        pyxel.cls(0)
+
+        if(self.state == "menu"):
+            ...
+        elif(self.state == "start"):
+            utils.draw_grid()
+            
+        elif(self.state == "prim"):
+            utils.draw_grid()
+
+            if utils.edges != []:
+                for i in range(0, utils.delay+1):
+                    utils.cave_paint(utils.edges[i][0], utils.edges[i][1])
+
+        elif(self.state == "run"):
+            utils.draw_grid()
+
+            for i in range(0, len(utils.edges)):
+                utils.cave_paint(utils.edges[i][0], utils.edges[i][1])
+            
+            player1.draw()
+
+        elif(self.state == "game_over"):
+            ...
+
--- paku/utils.py
+++ paku/utils.py
@@ -1,7 +1,27 @@
+import graph
+# import player
+
 import pyxel
 
 WIDTH = 256
 HEIGHT = 196
+
+g = graph.Graph()
+tree = graph.Graph()
+edges = []
+delay = 220
+
+
+def draw_grid():
+    for i in range(0, 18):
+        pyxel.line(15*i, 0, 15*i, HEIGHT, 7)
+
+    for i in range(0, 14):
+        pyxel.line(0, 15*i, WIDTH, 15*i, 7)
+        
+    for i in range(1, 17):
+        for j in range(1, 13):
+            pyxel.pset(15*i, 15*j, 8)
 
 def rect_custom(x1, y1, x2, y2, color):
 
@@ -21,6 +41,29 @@ def coord_int(coord):
     st = coord.split("-")
     return [int(st[0]), int(st[1])]
 
+def get_close_node(node: graph.Node, direction):
+    coords = coord_int(node.get_id())
+
+    bro_node = None
+
+    if(direction == "up"):
+        if(coords[1] != 0):
+            bro_node = g.get_node(coord_str(coords[0], coords[1]-1))
+
+    elif(direction == "left"):
+        if(coords[0] != 0):
+            bro_node = g.get_node(coord_str(coords[0]-1, coords[1]))
+            
+    elif(direction == "down"):
+        if(coords[1] != 12):
+            bro_node = g.get_node(coord_str(coords[0], coords[1]+1))
+            
+    elif(direction == "right"):
+        if(coords[0] != 16):
+            bro_node = g.get_node(coord_str(coords[0]+1, coords[1]))
+    
+    return bro_node.get_id()
+
 def cave_paint(current, bro):
     current_pos = coord_int(current)
     bro_pos = coord_int(bro)
@@ -38,6 +81,7 @@ def cave_paint(current, bro):
         y1 += 14
 
     rect_custom(x1, y1, x2, y2, 8)
+
 
 # DEBUG
 
